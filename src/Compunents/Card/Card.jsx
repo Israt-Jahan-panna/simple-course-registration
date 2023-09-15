8f05f2681de5a0c60c0bd74f4319bda510b07abe
Israt Jahan <isratjahanpanna87@gmail.com>
2023-09-15 22:43:20 +0600

    Done with tailwind setup in vite projects

---
+++ src/Compunents/Card/Card.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import  './card.css'
+const Card = () => {
+    return (
+        <div>
+            <h2>Card Compunent </h2>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/Compunents/Home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+           <h3 className='text-6xl ' >Hello World</h3> 
+        </div>
+        
+    );
+};
+
+export default Home;
